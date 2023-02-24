88e383c0caee6f542eef630a04287942851b4570
Pedro Salinas <pedrosalinas0899@gmail.com>
2023-02-24 19:45:58 -0300

    login con hash :U

--- utils/validate_login.php
+++ utils/validate_login.php
@@ -13,25 +13,33 @@ $con = connect();
 
 $email = $_POST['email'];
 $pass = $_POST['pass'];
+$pass_hash = password_hash($pass, PASSWORD_DEFAULT);
 
-$sql = 'SELECT * FROM usuario WHERE correo = ? AND password= ?'; # no
-$params = [$email, $pass];
+$sql = 'SELECT * FROM usuario WHERE correo = ?'; # no
+$params = [$email];
 
 $query = select($con, $sql, $params);
 
 if (count($query) > 0) {
-    $_SESSION['session'] = true;
-    $_SESSION['id'] = $query[0][0];
-    $_SESSION['name'] = $query[0][1];
-    $_SESSION['email'] = $query[0][2];
-    $_SESSION['pass'] = $query[0][3]; # NO
-    $_SESSION['rut'] = $query[0][4];
-    $_SESSION['admin'] = $query[0][5];
-    $_SESSION['ts'] = $query[0][6];
+    $pass_valid = password_verify($query[0][3], $pass_hash);
+    if ($pass_valid) {
+        $_SESSION['session'] = true;
+        $_SESSION['id'] = $query[0][0];
+        $_SESSION['name'] = $query[0][1];
+        $_SESSION['email'] = $query[0][2];
+        $_SESSION['pass'] = $pass_hash;
+        $_SESSION['rut'] = $query[0][4];
+        $_SESSION['admin'] = $query[0][5];
+        $_SESSION['ts'] = $query[0][6];
 
-    $_SESSION['validate'] = 0;
-    header('Location: ../index.php');
-    exit();
+        $_SESSION['validate'] = 0;
+        header('Location: ../index.php');
+        exit();
+    } else {
+        $_SESSION['validate'] = 1;
+        header('Location: ../login.php');
+        exit();
+    }
 } else {
     $_SESSION['validate'] = 1;
     header('Location: ../login.php');
